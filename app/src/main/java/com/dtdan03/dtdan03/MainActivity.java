d74ddcc15fc77e3511c01b707b6fe5ce869329f5
dtdan03 <78373263+dtdan03@users.noreply.github.com>
2021-10-26 21:18:40 +0200

    Initial commit

---
+++ app/src/main/java/com/dtdan03/dtdan03/MainActivity.java
@@ -0,0 +1,22 @@
+package com.dtdan03.dtdan03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+//author: dtdan03
+// Version 26102021_2058
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
